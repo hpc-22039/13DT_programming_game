f2e8263aa01d1f4ba3c9f444c574000a2731e06f
hpc-22039 <22039@haurakiplains.school.nz>
2024-08-27 11:34:01 +1200

    did manual level/room transition between 3 first rooms :)))

--- 13DT_programming_game/_13DT_programming_game.py
+++ 13DT_programming_game/_13DT_programming_game.py
@@ -17,26 +17,28 @@ start_bg = pygame.transform.scale(start_bg, (800, 800))
 player_surface = pygame.image.load('earth.png').convert_alpha()
 player_surface = pygame.transform.scale(player_surface, (50, 50)) 
 player_rect = player_surface.get_rect()
-landmark = pygame.Rect(400, 400, 100, 100)
+
 map_menu = pygame.image.load('game_map.png').convert_alpha()
 map_menu = pygame.transform.scale(map_menu, (1000, 600))
-#room_list = []
+
 copy_room = pygame.image.load('copy_room.png')
 periodicals_room = pygame.image.load('periodicals_room.png')
-#room_list.append(copy_room)
+toilets_room = pygame.image.load('toilets_room.png')
+
+p_to_co_door = pygame.Rect(340, 50, 50, 10)
+co_to_p_door = pygame.Rect(420, 680, 70, 10)
+co_to_t_door = pygame.Rect(580, 370, 10, 70)
+t_to_co_door = pygame.Rect(185, 365, 10, 70)
 
 class Room:
-    def __init__(self, x, y, width, height, image, north, east, south, west):
+    def __init__(self, x, y, width, height, image):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.image = pygame.transform.scale(image, (self.width, self.height))
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        self.north = north
-        self.east = east
-        self.south = south
-        self.west = west
+
         
     def draw_room(self):
         global draw_room
@@ -49,10 +51,11 @@ class Room:
             #screen.blit(room_surface, room_rect)   
 
 Room_List = []        
-CopyRoom = Room(200, 100, 400, 600, copy_room, False, True, True, False)
-Room_List.append(CopyRoom)
-PeriodicalsRoom = Room(200, 100, 500, 700, periodicals_room, True, False, False, True)
-Room_List.append(PeriodicalsRoom)
+
+PeriodicalsRoom = Room_List.append(Room(200, 50, 400, 700, periodicals_room))
+CopyRoom = Room_List.append(Room(200, 100, 400, 600, copy_room))
+ToiletsRoom = Room_List.append(Room(150, 100, 500, 600, toilets_room))
+
 
 #classes
 class LevelManager:
@@ -67,11 +70,12 @@ class LevelManager:
                 sys.exit()
             #if event.type == keys[pygame.K_KP_ENTER]:
             if event.type == pygame.MOUSEBUTTONDOWN:
-                self.state = 'main_game'
+                self.state = 'periodicals_level'
                 
         screen.blit(start_bg, (0,0))
 
-    def main_game(self):
+
+    def periodicals_level(self):
         for event in pygame.event.get():
         #quit program
             if event.type == pygame.QUIT:
@@ -80,18 +84,20 @@ class LevelManager:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_m:
                     self.state = 'map_menu'
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(pygame.mouse.get_pos())
 
         screen.fill((102, 54, 81)) 
         Room_List[0].draw_room()
         screen.blit(player_surface, player_rect)
-        pygame.draw.rect(screen, pygame.Color('red'), landmark)
-        #self.collision_detection() 
-        if player_rect.colliderect(landmark):
-            print('g')
-            player_rect.x = 20
-            self.state = 'main_game_2'
+        pygame.draw.rect(screen, pygame.Color('brown'), p_to_co_door) 
+        
+        if player_rect.colliderect(p_to_co_door):
+            player_rect.x = co_to_p_door.x
+            player_rect.y = co_to_p_door.y - 50
+            self.state = 'copy_level'
          
-    def main_game_2(self):
+    def copy_level(self):
         for event in pygame.event.get():
         #quit program
             if event.type == pygame.QUIT:
@@ -100,12 +106,45 @@ class LevelManager:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_m:
                     self.state = 'map_menu'
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(pygame.mouse.get_pos())
 
         screen.fill((102, 54, 81)) 
         Room_List[1].draw_room()
         screen.blit(player_surface, player_rect)
-        pygame.draw.rect(screen, pygame.Color('red'), landmark)
-        #self.collision_detection() 
+        pygame.draw.rect(screen, pygame.Color('brown'), co_to_p_door)
+        pygame.draw.rect(screen, pygame.Color('brown'), co_to_t_door)
+        
+        if player_rect.colliderect(co_to_p_door):
+            player_rect.x = p_to_co_door.x
+            player_rect.y = p_to_co_door.y + 50
+            self.state = 'periodicals_level'
+        elif player_rect.colliderect(co_to_t_door):
+            player_rect.x = t_to_co_door.x + 50
+            player_rect.y = t_to_co_door.y
+            self.state = 'toilets_level'
+
+    def toilets_level(self):
+        for event in pygame.event.get():
+        #quit program
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_m:
+                    self.state = 'map_menu'
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(pygame.mouse.get_pos())
+
+        screen.fill((102, 54, 81)) 
+        Room_List[2].draw_room()
+        screen.blit(player_surface, player_rect)
+        pygame.draw.rect(screen, pygame.Color('brown'), t_to_co_door)
+        
+        if player_rect.colliderect(t_to_co_door):
+            player_rect.x = co_to_t_door.x - 50
+            player_rect.y = co_to_t_door.y
+            self.state = 'copy_level'
 
         
     def map_menu(self):
@@ -118,22 +157,21 @@ class LevelManager:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_m:
                     self.state = 'main_game'
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(pygame.mouse.get_pos())       
                 
         screen.blit(map_menu, (0,0))
         
-    def collision_detection(self):
-        if player_rect.colliderect(landmark):
-            print('g')
-            self.state = 'start_menu'
-            player_rect.x = 20
   
     def level_manager(self):
         if self.state == 'start_menu':
             self.start_menu()
-        if self.state == 'main_game':
-            self.main_game()
-        if self.state == 'main_game_2':
-            self.main_game_2()
+        if self.state == 'periodicals_level':
+            self.periodicals_level()
+        if self.state == 'copy_level':
+            self.copy_level()
+        if self.state == 'toilets_level':
+            self.toilets_level()
         if self.state == 'map_menu':
             self.map_menu()
 
@@ -168,6 +206,7 @@ def player_movement(keys):
       
 
 while True:
+    
     keys = pygame.key.get_pressed()
 
     level_manager.level_manager()
